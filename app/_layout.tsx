aa27102af9d107df2e8239765d108375be4fc5c2
Muhamad Iqbal <mhmdiqbalabqi@gmail.com>
2025-06-09 07:12:31 +0700

    refactor: update ESLint configuration and add plugins for improved code quality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
-import useCustomFonts from '@/hooks/useCustomFonts';
-import queryClient from '@/lib/query-client';
+import 'react-native-reanimated';
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
-import 'react-native-reanimated';
+
+import useCustomFonts from '@/hooks/useCustomFonts';
+import queryClient from '@/lib/query-client';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const { loaded } = useCustomFonts()
+  const { loaded } = useCustomFonts();
 
   if (!loaded) {
     return null;
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,40 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const react = require("eslint-plugin-react");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
+const typeScriptSortKeys = require("eslint-plugin-typescript-sort-keys");
+const sortDestructureKeys = require("eslint-plugin-sort-destructure-keys");
+const sortKeysFix = require("eslint-plugin-sort-keys-fix");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    plugins: {
+      react,
+      "simple-import-sort": simpleImportSort,
+      "sort-destructure-keys": sortDestructureKeys,
+      "sort-keys-fix": sortKeysFix,
+      "typescript-sort-keys": typeScriptSortKeys,
+    },
+    rules: {
+      indent: ["error", 2],
+      "react/jsx-sort-props": [
+        "error",
+        {
+          callbacksLast: true,
+          ignoreCase: true,
+          multiline: "last",
+          shorthandFirst: true,
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": ["error"],
+      "sort-destructure-keys/sort-destructure-keys": 2,
+      "sort-keys-fix/sort-keys-fix": "warn",
+      "typescript-sort-keys/interface": "error",
+      "typescript-sort-keys/string-enum": "error",
+    },
   },
 ]);
