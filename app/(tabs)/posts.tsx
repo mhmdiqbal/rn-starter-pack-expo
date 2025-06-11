8a7702ccd7f47da3429ea9c13324bb6478847f99
Muhamad Iqbal <mhmdiqbalabqi@gmail.com>
2025-06-11 13:38:44 +0700

    feat: wrap FlatList in SafeAreaView for improved layout and styling

--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { usePosts } from '@/features/home/queries';
+import tw from '@/lib/tailwind';
 
 export default function PostsScreen() {
   const { data, error, isLoading } = usePosts();
@@ -12,14 +14,16 @@ export default function PostsScreen() {
   if (error) return <Text>Error loading users.</Text>;
 
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View style={{ borderBottomWidth: 1, borderColor: '#eee', padding: 16 }}>
-          <Text>{item.title}</Text>
-        </View>
-      )} />
+    <SafeAreaView style={tw`flex-1 bg-white`}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ borderBottomWidth: 1, borderColor: '#eee', padding: 16 }}>
+            <Text>{item.title}</Text>
+          </View>
+        )} />
+    </SafeAreaView>
   );
 }
 
