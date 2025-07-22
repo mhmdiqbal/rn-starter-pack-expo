406c2342a41ac2ecc0eea017dc2dcf078c6a1244
Muhamad Iqbal <mhmdiqbalabqi@gmail.com>
2025-07-22 07:52:00 +0700

    feat: implement ButtonBase component with styles and ripple configuration

---
+++ components/button-base/button-base.tsx
@@ -0,0 +1,19 @@
+import { Pressable } from "react-native";
+
+import { RIPPLE_CONFIG } from "./button-base.cons";
+import styles from "./button-base.styles";
+import { ButtonBaseProps } from "./button-base.types";
+
+export default function ButtonBase({ style, ...props }: ButtonBaseProps) {
+  return (
+    <Pressable
+      android_ripple={RIPPLE_CONFIG}
+      style={({ hovered, pressed }) => [
+        styles.container(pressed),
+        typeof style === "function" ? style({ hovered, pressed }) : style,
+      ]}
+      {...props}/>
+  );
+}
+
+export type { ButtonBaseProps } from "./button-base.types";
